d03631cfd9f01abedd44444b1c91346796f8042d
Felix Titov <56549889+filtitov2001@users.noreply.github.com>
2021-09-23 17:33:48 +0300

    Update PlayState.java

--- core/src/com/mygdx/game/states/PlayState.java
+++ core/src/com/mygdx/game/states/PlayState.java
@@ -10,7 +10,7 @@ import com.mygdx.game.MyGame;
 import com.mygdx.game.sprites.Bird;
 import com.mygdx.game.sprites.Tube;
 
-public class PlayState extends State{
+public class PlayState extends State {
 
     public static final int TUBE_SPACING = 125;
     public static final int TUBE_COUNT = 4;
@@ -26,17 +26,18 @@ public class PlayState extends State{
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
+        
         bird = new Bird (50,300);
         camera.setToOrtho(false, MyGame.WIDTH/2,MyGame.HEIGHT/2);
         bg = new Texture("background.png");
         cloud = new Texture("cloud.png");
+        
         cloudPos1 = new Vector2(camera.position.x - camera.viewportWidth / 2, GROUND_Y_OFFSET);
         cloudPos2 = new Vector2((camera.position.x - camera.viewportWidth / 2) + cloud.getWidth(), GROUND_Y_OFFSET);
 
         tubes = new Array<Tube>();
 
-        for(int i =0;i<TUBE_COUNT;i++)
-        {
+        for(int i =0;i<TUBE_COUNT;i++) {
             tubes.add(new Tube(i*(TUBE_SPACING + Tube.TUBE_WIDTH)));
         }
 
@@ -44,8 +45,7 @@ public class PlayState extends State{
 
     @Override
     public void handleInput() {
-        if (Gdx.input.justTouched())
-        {
+        if (Gdx.input.justTouched()) {
             bird.jump();
         }
 
@@ -58,8 +58,7 @@ public class PlayState extends State{
         bird.update(dt);
         camera.position.x = bird.getPosition().x +80;
 
-        for(int i = 0;i<tubes.size;i++)
-        {
+        for(int i = 0;i<tubes.size;i++) {
             Tube tube = tubes.get(i);
             if(camera.position.x - (camera.viewportWidth/2) > tube.getPosTopTube().x
                 + tube.getTopTube().getWidth()){
@@ -78,8 +77,7 @@ public class PlayState extends State{
         sb.begin();
         sb.draw(bg,camera.position.x-(camera.viewportWidth/2)-50,-500);
         sb.draw(bird.getBird(),bird.getPosition().x,bird.getPosition().y);
-        for (Tube tube: tubes)
-        {
+        for (Tube tube: tubes) {
             sb.draw(tube.getTopTube(),tube.getPosBotTube().x,tube.getPosTopTube().y);
             sb.draw(tube.getBottomTube(),tube.getPosBotTube().x,tube.getPosBotTube().y);
         }
@@ -94,11 +92,9 @@ public class PlayState extends State{
     public void dispode() {
         bg.dispose();
         bird.dispose();
-        for(Tube tube:tubes)
-        {
+        for(Tube tube:tubes) {
             tube.dispose();
         }
-
 
     }
 
