de2cf3f6a8d2311c09dc2b68f506d1aabadff9a1
Felix Titov <56549889+filtitov2001@users.noreply.github.com>
2021-09-23 17:34:10 +0300

    Update State.java

--- core/src/com/mygdx/game/states/State.java
+++ core/src/com/mygdx/game/states/State.java
@@ -4,14 +4,12 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
 
-public abstract class State
-{
+public abstract class State {
     protected OrthographicCamera camera;
     protected Vector3 mouse;
     protected GameStateManager gsm;
 
-    public State(GameStateManager gsm)
-    {
+    public State(GameStateManager gsm) {
         this.gsm = gsm;
         camera = new OrthographicCamera();
         mouse = new Vector3();
@@ -22,7 +20,5 @@ public abstract class State
     public abstract void update(float dt);
     public abstract void render(SpriteBatch sb);
     public abstract void dispode();
-
-
 
 }
