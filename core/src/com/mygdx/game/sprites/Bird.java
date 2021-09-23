b949e4eb0c7be198ad540d6d25d46b95ea171729
Felix Titov <56549889+filtitov2001@users.noreply.github.com>
2021-09-23 17:35:48 +0300

    Update Bird.java

--- core/src/com/mygdx/game/sprites/Bird.java
+++ core/src/com/mygdx/game/sprites/Bird.java
@@ -8,36 +8,30 @@ import java.lang.reflect.Modifier;
 
 public class Bird {
     public static final int MOVEMENT = 100;
-
     public static final int GRAVITY = -15;
+    
     private Vector3 position;
     private Vector3 velocity;
     private Rectangle bounds;
 
     private Texture bird;
 
-
-
-    public Bird(int x, int y)
-    {
+    public Bird(int x, int y) {
         position = new Vector3(x,y,0);
         velocity = new Vector3(0,0,0);
         bird = new Texture("bird.png");
         bounds = new Rectangle(x,y,bird.getWidth(),bird.getHeight());
     }
 
-    public void update(float dt)
-    {
-        if(position.y > 0)
-        {
+    public void update(float dt) {
+        if(position.y > 0) {
             velocity.add(0,GRAVITY,0);
         }
 
         velocity.add(0,GRAVITY,0);
         velocity.scl(dt);
         position.add(MOVEMENT*dt,velocity.y,0);
-        if(position.y <0)
-        {
+        if(position.y <0) {
             position.y =0 ;
         }
 
@@ -46,6 +40,7 @@ public class Bird {
 
 
     }
+    
     public Vector3 getPosition() {
         return position;
     }
@@ -54,8 +49,7 @@ public class Bird {
         return bird;
     }
 
-    public void jump()
-    {
+    public void jump() {
         velocity.y = 350;
     }
 
